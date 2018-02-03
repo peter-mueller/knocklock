35b2aadb0b459fc07a2ef2547df2add4efc78914
peter-mueller <peter7995@gmail.com>
2018-02-03 19:59:38 +0100

    fehlerarten

--- data/studie qualitativ/qualitative-plots.R
+++ data/studie qualitativ/qualitative-plots.R
@@ -1,7 +1,9 @@
 data = read.csv("formatiert.csv", sep=";")
 
 studie = sample(data)
+
 summary(studie)
+length(studie$Person)
 BewertungEingabe = factor(studie$BewertungEingabe ,levels=c(1,2,3,4,5), labels=c("Sehr leicht","-",".","+","Sehr schwer"))
 BewertungSicherheit = factor(studie$BewertungSicherheit ,levels=c(1,2,3,4,5), labels=c("Nicht sicher","-",".","+","Sehr sicher"))
 ProblemeSpeichern = factor(studie$ProblemeSpeichern, levels=c(1,2,3,4,5), labels=c("Keine","-",".","+","Viele"))
@@ -11,18 +13,18 @@ BevorzugenVorPasswort= factor(studie$BevorzugenVorPasswort, levels=c(1,2,3,4,5),
 
 summary(BewertungEingabe )
 BewertungSicherheit
-par(mfrow=c(3,2))
+par(mfrow=c(3,2), cex=1.2)
 
 
-t = table(BewertungEingabe,useNA)
+t = table(BewertungEingabe)
 nas = paste(sum(is.na(BewertungEingabe)), "nicht angegeben")
 barplot(t, main="Wie bewertest du die Eingabe?", sub=nas )
 
-t = table(BewertungSicherheit,useNA)
+t = table(BewertungSicherheit)
 nas = paste(sum(is.na(BewertungSicherheit)), "nicht angegeben")
 barplot(t, main="Wie bewertest du die Sicherheit \n des KnockLocks?", sub=nas)
 
-t = table(ProblemeSpeichern,useNA)
+t = table(ProblemeSpeichern)
 nas = paste(sum(is.na(ProblemeSpeichern)), "nicht angegeben")
 barplot(t, main="Gab es Probleme beim Speichern \n deines Musters?", sub=nas)
 
@@ -40,3 +42,16 @@ barplot(t, main="Gab es Probleme beim Entsperren?", sub=nas)
 par(mfrow=c(1,1))
 nas = paste(sum(is.na(studie$Alter)), "nicht angegeben")
 hist(studie$Alter, main="Wie alt bist du?", sub=nas)
+
+# Fehlerarten
+
+tab = matrix( 
+   c(1114,0,0,0, 0,466,189,188), 
+   ncol=2
+)
+rownames(tab) <- c("Erfolgreich",  "Falsche Anzahl","Zu große Zeitabweichung", "Zu große Abweichung")
+colnames(tab) <- c("Erfolgreich", "Fehler")
+tab
+
+
+barplot(tab, legend=rownames(tab), col=c("green","red", "orange", "yellow"))
